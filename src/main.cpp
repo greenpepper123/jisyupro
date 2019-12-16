a37c0e63963ca73756be3c511384a30163f98d30
Nobuki Sugito <nobupiiman@gmail.com>
2019-12-16 18:24:34 +0900

    publish correct LaserScan and display with rviz O.K.

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,8 @@ Device device;
 int main() {
     // initialize
     int len,i,distance,pos=0,pos_new;
-    std::vector<float> ranges_to_send;
-    bool send_ready = false, counting_up = false;
+    std::vector<float> ranges_to_send, distances;
+    bool send_ready = false, counting_up = false, valid = true;
     device.init();
 
 #ifdef ENABLE_VIZ
@@ -35,15 +35,17 @@ int main() {
         if (send_ready) {
             if (counting_up) {
                 for (int i = 0; i < 15; i++) {
-                    ranges_to_send.push_back(points[i][0] / 1000.0);
+                    ranges_to_send.push_back(distances[i] / 1000.0);
                 }
             } else {
                 for (int i = 14; i >= 0; i--) {
-                    ranges_to_send.push_back(points[i][0] / 1000.0);
+                    ranges_to_send.push_back(distances[i] / 1000.0);
                 }
             }
             node.publish(ranges_to_send);
             points.clear();
+            distances.clear();
+            ranges_to_send.clear();
             send_ready = false;
             node.first_point_stamp = ros::Time::now();
         }
@@ -60,15 +62,32 @@ int main() {
             distance+=((device.buf[len-i]-48)*std::pow(10.0, i-4));
         }
         pos_new = device.buf[len-3];
-        if (pos_new == 8 && pos == 7) {
-            counting_up = true;
-        } else if (pos_new == 7 && pos == 8) {
-            counting_up = false;
+        if (distance < 5) {
+            valid = false;
+        }
+        if ((not valid) and (pos_new == pos)) {
+            points.clear();
+            distances.clear();
+            valid = true;
+        }
+        if (valid) {
+            if (pos_new == 8 && pos == 7) {
+                counting_up = true;
+            } else if (pos_new == 7 && pos == 8) {
+                counting_up = false;
+            }
+            distances.push_back(distance);
+            points.push_back({distance/300.0f*sinf32(2.0*pos_new*M_PI/45.0), distance/300.0f*cosf32(2.0*pos_new*M_PI/45.0)});
+            if (points.size() > 14) {
+                if ((pos_new == 14 and pos == 13) or (pos_new == 0 and pos == 1)) {
+                    send_ready = true;
+                } else {
+                    valid = false;
+                }
+            }
         }
         pos = pos_new;
-        points.push_back({distance/300.0f*sinf32(2.0*pos*M_PI/45.0), distance/300.0f*cosf32(2.0*pos*M_PI/45.0)});
-        if (points.size() > 14) { send_ready = true; }
-        //std::cout<<points.size()<<" "<<pos<<" "<<distance<<std::endl;
+        std::cout<<points.size()<<" "<<pos<<" "<<distance<<std::endl;
     }
 
     return 0;
